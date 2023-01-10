fe9e234b7db8ae223c1b78c6e1daaa202945655e
soharddbwarez <penders.ricardo@gmail.com>
2023-01-10 01:34:29 +0100

    worked on my DC Load code using TcMenu
    Signed-off-by: soharddbwarez <penders.ricardo@gmail.com>

---
+++ TcMenuDCLoad/src/TcMenuDCLoad.ino
@@ -0,0 +1,52 @@
+#include "TcMenuDCLoad_menu.h"
+
+void setup() {
+    setupMenu();
+
+}
+
+void loop() {
+    taskManager.runLoop();
+
+}
+
+
+void CALLBACK_FUNCTION TogglePin13(int id) {
+    // TODO - your menu change code
+}
+
+
+void CALLBACK_FUNCTION SelectControl(int id) {
+    // TODO - your menu change code
+}
+
+
+void CALLBACK_FUNCTION GetAmps(int id) {
+    // TODO - your menu change code
+}
+
+
+void CALLBACK_FUNCTION SetScreenBrightness(int id) {
+    // TODO - your menu change code
+}
+
+
+void CALLBACK_FUNCTION SetMaxP(int id) {
+    // TODO - your menu change code
+}
+
+
+void CALLBACK_FUNCTION SetMaxA(int id) {
+    // TODO - your menu change code
+}
+
+
+
+void CALLBACK_FUNCTION SetMaxV(int id) {
+    // TODO - your menu change code
+}
+
+
+void CALLBACK_FUNCTION GetVoltage(int id) {
+    // TODO - your menu change code
+}
